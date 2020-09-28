fd5fecaa8bc05d3d6439399e21050edcbd65b23d
SukhmaniKaur <sukhmanikaur.appknit@gmail.com>
2020-09-28 12:30:03 +0530

    Initial Commit

---
+++ ChatWihSockets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatWihSockets
+//
+//  Created by MACBOOK on 28/09/20.
+//  Copyright © 2020 SukhmaniKaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
